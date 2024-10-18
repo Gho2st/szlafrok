5375ac7c05e7707b2be279359bf6ac09183648a7
Dominik <dominik.jojczyk@gmail.com>
2024-10-18 16:02:48 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ import Footer from "@/components/UI/Footer";
 export const metadata = {
   title: "SzlafrO.K. - Szlafroki i Piżamy dla Każdego",
   description:
-    "Szlafroki Kraków. Od 1990r zapraszamy do naszych specjalistyczego sklepu ze szlafrokami w Krakowie przy ulicy Starowiślnej 19. Oprócz szlafroków w swojej ofercie posiadamy również duży wybór piżam, koszuli nocnych, szlafroków i piżam dla dzieci.",
+    "Szlafroki Kraków. Od 1990r zapraszamy do naszych specjalistyczego sklepu ze szlafrokami w Krakowie przy ulicy Starowiślnej 19. Oprócz szlafroków w swojej ofercie posiadamy również duży wybór piżam, koszul nocnych, szlafroków i piżam dla dzieci.",
   alternates: {
     canonical: "https://www.szlafroki.krakow.pl/",
   },
