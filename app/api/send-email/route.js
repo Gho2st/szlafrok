242f7d58fa863b68dcc60c61ebe6e0b64e314576
Dominik <dominik.jojczyk@gmail.com>
2024-12-08 20:33:05 +0100

    Update route.js

--- app/api/send-email/route.js
+++ app/api/send-email/route.js
@@ -57,8 +57,7 @@ export async function POST(request) {
 
     const mailOption = {
       from: "biosite.praca@gmail.com",
-      // to: "mbarciewicz@onet.eu",
-      to: "domiweb.biuro@gmail.com",
+      to: "mbarciewicz@onet.eu",
       subject: "Email ze strony SzlafrOK od klienta",
       html: `
         <div style="font-family: Arial, sans-serif; color: #333; padding: 20px;">
